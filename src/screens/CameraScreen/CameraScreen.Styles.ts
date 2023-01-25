2db1703d5b568dd84c7f96347ccb4a32961529ae
bacayo <batuhanyolcuu@gmail.com>
2023-01-25 11:42:13 +0300

    added camera screen and camera button

--- src/screens/CameraScreen/CameraScreen.Styles.ts
+++ src/screens/CameraScreen/CameraScreen.Styles.ts
@@ -4,4 +4,34 @@ export default StyleSheet.create({
   container: {
     flex: 1,
   },
+  cameraView: {
+    flex: 1,
+  },
+  cameraButtonWrapper: {
+    position: 'absolute',
+    bottom: '5%',
+    gap: 18,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+  },
+  cameraButtonTitleWrapper: {
+    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+    paddingHorizontal: 18,
+    paddingVertical: 4,
+    alignSelf: 'center',
+    borderRadius: 25,
+  },
+  buttonTitle: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  cameraButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'white',
+    borderRadius: 50,
+    borderWidth: 2,
+    borderColor: 'black',
+  },
 });
--- src/screens/CameraScreen/CameraScreen.tsx
+++ src/screens/CameraScreen/CameraScreen.tsx
@@ -1,14 +1,69 @@
-import {View} from 'react-native';
-import React from 'react';
+import {View, Linking, Text} from 'react-native';
+import React, {useCallback, useEffect} from 'react';
 
 import styles from './CameraScreen.Styles';
 import Navbar from '../../components/Navbar';
+import {
+  useCameraDevices,
+  Camera,
+  CameraDevice,
+} from 'react-native-vision-camera';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {FeedStackParamList} from '../../infrastructure/types/nav.types';
 
 const CameraScreen = () => {
+  // const devices = useCameraDevices('wide-angle-camera');
+  const devices = useCameraDevices();
+  const device: CameraDevice | undefined = devices.back;
+
+  const navigation =
+    useNavigation<NativeStackNavigationProp<FeedStackParamList>>();
+
+  useEffect(() => {
+    requestCameraPermission();
+  }, []);
+
+  // Camera permission
+  const requestCameraPermission = useCallback(async () => {
+    const permission = await Camera.requestCameraPermission();
+
+    if (permission === 'denied') {
+      await Linking.openSettings();
+    }
+  }, []);
+
+  if (device == null) {
+    return (
+      <View>
+        <Text>Loading</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       {/* Navbar */}
-      <Navbar iconName="cross" navbarText="New Post" onPress={null} />
+      <Navbar
+        iconName="cross"
+        navbarText="New Post"
+        onPress={() => {
+          navigation.navigate('Feed');
+        }}
+      />
+      {/* Camera */}
+      <Camera
+        style={styles.cameraView}
+        device={device}
+        isActive={true}
+        enableZoomGesture
+      />
+      <View style={styles.cameraButtonWrapper}>
+        <View style={styles.cameraButtonTitleWrapper}>
+          <Text style={styles.buttonTitle}>Add Photo</Text>
+        </View>
+        <View style={styles.cameraButton} />
+      </View>
     </View>
   );
 };
